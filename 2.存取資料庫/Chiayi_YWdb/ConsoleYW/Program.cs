39f18f606ba19a018f79e56aa5ee6ff2c43e618e
ASUS <ASUS@DESKTOP-BOTFKU2>
2017-04-08 19:10:19 +0800

    add 2.存取資料庫

---
+++ 2.存取資料庫/Chiayi_YWdb/ConsoleYW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YW.Models;
+
+namespace YW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var import = new YW.Service.ImportService();
+            var db = new YW.Repository.StationRepsitory();
+            var stations = import.FindStationData(); //xml
+            //stations
+            //    .ToList().ForEach(station =>
+            //{
+            //    db.Create(stations);
+            //});
+            //var stations = db.FindAllStations();
+            ShowStationData(stations);
+            Console.ReadLine();
+        }
+        public static void ShowStationData(List<StationData> stations)
+        {
+            Console.WriteLine(string.Format("共收到{0}筆監測站的資料", stations.Count));
+            stations.ForEach(x =>
+            {
+                Console.WriteLine(string.Format("站點名稱：{0}，地址:{1}", x.Site, x.Address));
+            });
+        }
+    }
+}
---
+++ 2.存取資料庫/Chiayi_YWdb/Models/Repository/StationRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YW.Repository
+{
+    public class StationRepsitory
+    {
+        private const string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\檔案\多媒體系統\0331_homework\Chiayi_YWdb\ConsoleYW\App_Data\MotorcycleDB.mdf;Integrated Security=True";
+        public void Create(List<Models.StationData> stations)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection();
+            connection.ConnectionString = _connectionString;
+            connection.Open();
+            foreach(var station in stations)
+            {
+                var command = new System.Data.SqlClient.SqlCommand("", connection);
+                command.CommandText = string.Format(@"
+INSERT          INTO    Motorcycle(Siteid, Site, Address, Phonenumber)
+VALUES          (N'{0}',N'{1}',N'{2}',N'{3}')
+", station.Siteid, station.Site, station.Address, station.Phonenumber);
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+        }
+        public List<Models.StationData> FindAllStations()
+        {
+            var result = new List<Models.StationData>();
+            var connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+            connection.ConnectionString = _connectionString;
+            connection.Open();
+            var command = new System.Data.SqlClient.SqlCommand("", connection);
+            command.CommandText = @"
+Select * from Motorcycle";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Models.StationData item = new Models.StationData();
+                item.Siteid = reader["Siteid"].ToString();
+                item.Site = reader["Site"].ToString();
+                item.Address = reader["Address"].ToString();
+                item.Phonenumber = reader["Phonenumber"].ToString();
+                result.Add(item);
+            }
+            connection.Close();
+            return result;
+        }
+    }
+}
