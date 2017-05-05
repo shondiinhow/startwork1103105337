04e6c439b8f6d78933fbc9fe0c4f222cb4b55c20
ASUS <ASUS@DESKTOP-BOTFKU2>
2017-05-06 00:26:38 +0800

    add html 5/5

--- 3.Web範例/Chiayi_YWweb/Web/Controllers/DemoContentController.cs
+++ 3.Web範例/Chiayi_YWweb/Web/Controllers/DemoContentController.cs
@@ -9,7 +9,7 @@ namespace Web.Controllers
     public class DemoContentController : Controller
     {
         // GET: DemoContent
-        public ActionResult Index()
+        public ActionResult IndexMessage()
         {
             var stationRepository = new YW.Repository.StationRepsitory();
             var stations = stationRepository.FindAllStations();
@@ -19,6 +19,18 @@ namespace Web.Controllers
                 message += string.Format("站點名稱：{0}，地址：{1}<br/>", x.Site, x.Address);
             });
             return Content(message);
+        }
+        public ActionResult Index(string userName = "")
+        {
+            var stationRepository = new YW.Repository.StationRepsitory();
+            var stations = stationRepository.FindAllStations();
+            //var message = string.Format("共收到{0}筆監測站的資料<br/>", stations.Count);
+            //stations.ForEach(x =>
+            //{
+            //    message += string.Format("站點名稱：{0}，地址：{1}<br/>", x.Site, x.Address);
+            //});
+            ViewBag.UserName = userName;
+            return View(stations);
         }
     }
 }
